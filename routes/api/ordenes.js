f5a507dfa2cfb1c5ac9a4fcf789984f31e572160
Miguel Angel <mangelmcho@gmail.com>
2018-11-29 15:10:24 -0400

    send pdf and download guide for delivery

--- routes/api/ordenes.js
+++ routes/api/ordenes.js
@@ -64,18 +64,9 @@ router.post('/', function (req, res, next) {
 });
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
+var nodemailer = require('nodemailer'); // email sender function
 router.get('/facturas/:id', function (req, res, next) {
 
-    /*var html = '<div id="pageHeader">Default header</div>'+
-    '<div id="pageHeader-first">Header on first page</div>'+
-    '<div id="pageHeader-2">Header on second page</div>'+
-    '<div id="pageHeader-3">Header on third page</div>'+
-    '<div id="pageHeader-last">Header on last page</div>'+
-    
-    var footer = '<div id="pageFooter">Default footer</div>'+
-    '<div id="pageFooter-first">Footer on first page</div>'+
-    '<div id="pageFooter-2">Footer on second page</div>'+
-    '<div id="pageFooter-last">Footer on last page</div>'*/
 
     Orden.findById(req.params.id).populate('restaurant').populate('menus').populate('cliente').exec()
         .then(doc => {
@@ -84,24 +75,20 @@ router.get('/facturas/:id', function (req, res, next) {
 
             pdf = new PDFDocument
 
-            let idOrden = req.params.id
-            pdf.pipe(fs.createWriteStream(idOrden + '.pdf'));
-
-
-
+            let idOrden = req.params.id;
+            let writeStream = fs.createWriteStream(idOrden + '.pdf');
+            pdf.pipe(writeStream);
             // Add another page 
-            pdf.addPage()
-                .fontSize(25)
-                .text('Id de Factura : ' + idOrden, 100, 100)
 
+            pdf
+                .fontSize(20)
+                .text('Id de Factura : ' + idOrden, 100, 100)
                 .moveDown()
-            pdf.text('Nombre o Razon Social ' + doc.cliente.nombre, {
+
+            pdf.fontSize(12).text('Nombre o Razon Social ' + doc.cliente.nombre, {
                 width: 412,
                 align: 'left'
             })
-
-
-
             pdf.moveDown()
             pdf.text('Correo electronico : ' + doc.cliente.email, {
                 width: 412,
@@ -113,15 +100,16 @@ router.get('/facturas/:id', function (req, res, next) {
                 align: 'left'
             })
             pdf.moveDown()
+            //pdf.rect(pdf.x, 0, 410, pdf.y).stroke()
+
+
             pdf.text('Telefono :  ' + doc.cliente.nombre, {
                 width: 412,
                 align: 'left'
             })
             pdf.moveDown()
 
-
-
-            pdf.text('DETALLE DE PEDIDO' + doc.cliente.nombre, {
+            pdf.text('DETALLE DE PEDIDO', {
                 width: 412,
                 align: 'center'
             })
@@ -145,27 +133,38 @@ router.get('/facturas/:id', function (req, res, next) {
                 width: 412,
                 align: 'left'
             })
-
-
             pdf.moveDown()
-            pdf.text('Nombre ----------- Precio ', {
+            //image
+            /* pdf.image('out2.png', pdf.x, pdf.y, {
+                width: 300
+            }) */
+
+            pdf.text('Nombre \n Precio \n Cantidad', {
                 width: 412,
+                height: 15,
+                columns: 3,
                 align: 'left'
             })
+            pdf.moveTo(95, pdf.y)
+                .lineTo(510, pdf.y).stroke()
+
             pdf.moveDown()
+            console.log(pdf.x, pdf.y);
+            pdf.rect(pdf.x - 5, pdf.y, 410, doc.menus.length * 20).stroke()
 
             for (let index = 0; index < doc.menus.length; index++) {
-
-                pdf.text(doc.menus[index].nombre + '-------' + doc.menus[index].precio + '------- ' + doc.cantidad[index], {
+                //pdf.rect(pdf.x, pdf.y, 410, 15).stroke()
+                pdf.text(doc.menus[index].nombre + '\n' + doc.menus[index].precio + '\n' + doc.cantidad[index], {
                     width: 412,
-                    align: 'left'
+                    align: 'left',
+                    height: 15,
+                    columns: 3
                 })
                 pdf.moveDown()
             }
-
             pdf.text('Total :  ' + doc.pagoTotal, {
                 width: 412,
-                align: 'center'
+                align: 'right'
             })
             pdf.moveDown()
 
@@ -186,19 +185,136 @@ router.get('/facturas/:id', function (req, res, next) {
 
             //pdf.pipe(res.status(201));
 
-            res.status(500).json(doc);
+            //res.status(500).json();
 
             //enviar el pdf al correo del cliente .
+
+            let config = JSON.parse(fs.readFileSync("config.json"))
+            //console.log(config.password);
+
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                secure: false,
+                port: 25,
+                auth: {
+
+                    user: 'maangelmcho@gmail.com', //su correo ,del que se enviara el email
+                    pass: config.password //aqui va la contraseña de su correo
+
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
+            var mailOptions = {
+                from: 'Api Rest Store!',
+                to: 'mangelmcho@gmail.com',
+                subject: 'Factura por servicio',
+                text: 'Adjuntamos la factura por servicio de comidas',
+                attachments: [{
+                    path: "./" + idOrden + ".pdf"
+                }]
+            };
+
+            writeStream.on('finish', function () {
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                        res.status(500).json({
+                            error: error
+                        });
+                    } else {
+
+
+                        pdf = new PDFDocument
+
+
+                        let writeStreamG = fs.createWriteStream(idOrden + '.pdf');
+                        pdfg.pipe(writeStreamG);
+                        // Add another page 
+
+                        pdfg
+                            .fontSize(20)
+                            .text('Id de Factura : ' + idOrden, 100, 100)
+                            .moveDown()
+
+                        pdfg.fontSize(12).text('Nombre o Razon Social ' + doc.cliente.nombre, {
+                            width: 412,
+                            align: 'left'
+                        })
+                        pdfg.image('out2.png', pdfg.x, pdfg.y, {
+                            width: 300
+                        })
+                        pdfg.end()
+
+                        writeStreamG.on('finish', function () {
+                            res.status(200).download('./' + idOrden + '.pdf');
+                        });
+                        console.log('done...!');
+
+                    }
+                });
+
+            });
+
         }).catch(err => {
             res.status(500).json({
-                error: err
+                error: err || "error"
             });
         });
 
 
     //doc.pipe(res.status(201));
-})
+});
 
+
+const staticmap = require("staticmap");
+
+router.get('/maps', function (req, res, next) {
+
+
+    /*staticmap.getMap(staticmap.png({
+            width: 500,
+            height: 500
+        }), 45.4724, -73.4520, 12)
+        .then((image) => {
+            image.save('out1.png');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+    staticmap.getBox(staticmap.png({
+            width: 500,
+            height: 500
+        }), 48.436034, 10.684891, 48.295985, 11.042633)
+        .then((image) => {
+            image.save('out2.png');
+        })
+        .catch((err) => {
+            console.log(err);
+        });*/
+
+    staticmap.getMap(staticmap.png({
+            width: 700,
+            height: 700,
+        }), -19.56604, -65.76899, 17)
+        .then((image) => {
+            //drawLine(x1, y1, x2, y2, color)
+            image.drawLine(340, 340, 360, 340, "#ffffff");
+            image.drawLine(340, 360, 360, 360, "#ffffff");
+            image.drawLine(340, 340, 340, 360, "#ffffff");
+            image.drawLine(360, 340, 360, 360, "#ffffff");
+            image.drawLine(340, 340, 360, 360, "#ffffff");
+            image.drawLine(360, 340, 340, 360, "#ffffff");
+            image.drawLine(0, 30, 350, 360, "#ffffff");
+
+            image.save('out2.png');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 module.exports = router;
 
 /*{
